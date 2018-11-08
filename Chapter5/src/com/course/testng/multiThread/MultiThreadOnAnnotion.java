9f520b2e80d1dc2dde1f77ec87d3f39f6a390e60
zhangfuxing <4452907148@qq.com>
2018-11-08 18:18:33 +0800

    chapter7moco

--- Chapter5/src/com/course/testng/multiThread/MultiThreadOnAnnotion.java
+++ Chapter5/src/com/course/testng/multiThread/MultiThreadOnAnnotion.java
@@ -1,4 +1,11 @@
 package course.testng.multiThread;
 
+import org.testng.annotations.Test;
+
 public class MultiThreadOnAnnotion {
+    @Test(invocationCount = 10,threadPoolSize = 3)
+    public void test1(){
+        System.out.println("123");
+        System.out.printf("Thread id: %s%n",Thread.currentThread().getId());
+    }
 }
